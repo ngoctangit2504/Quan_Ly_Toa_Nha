ede67f7f692db4d77ec4b3bc3c4cf4fc8fbaf218
ngoctangit2504 <lulu25042001@gmail.com>
2025-02-18 15:29:39 +0700

    xóa housegroup

--- housegroup/src/main/java/com/housegroup/housegroup/repository/HouseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.housegroup.housegroup.repository;
-
-import com.housegroup.housegroup.entity.House;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HouseRepository extends JpaRepository<House, Long> {
-}
